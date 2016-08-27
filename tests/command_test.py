f96f3baff05ff9556ce802b8964631badb2efaa4
Thomas Hobohm <superman3275@gmail.com>
2016-08-27 12:32:33 -0500

    Changed name of command test to reflect logdb testing.

--- tests/command_test.py
+++ tests/command_test.py
@@ -5,7 +5,7 @@ import unittest
 class SqliteTest(unittest.TestCase):
     TESTING_DIR = os.path.join(os.path.dirname(__file__), 'testing_dir')
 
-    def test_command(self):
+    def test_command_and_logdb_defaults(self):
         command = 'diary generate sqlite %s/log.sqlite3' % self.TESTING_DIR
         os.system(command)
 
